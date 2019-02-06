7e50f6ec7517ccc603f9e3480e5b4775082d7dc4
Igor Rud <rud.igor23@gmail.com>
2019-02-06 22:29:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,7 @@ def game_hash
   {
     :home => {
       :team_name => "Brooklyn Nets",
-      :colors => ["Black", "white"],
+      :colors => ["Black", "White"],
       :players => [
         {
         :player_name => "Alan Anderson",
