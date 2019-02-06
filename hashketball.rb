62c78df1ec6787b85085dbbaded785eca70dccc5
Igor Rud <rud.igor23@gmail.com>
2019-02-06 22:41:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -151,3 +151,10 @@ def team_names
     team[:team_name]
   end
 end
+
+def player_numbers(team_name)
+  find_numbers = team_stats.find {|team| team.fetch(:team_name) == team_name}
+  find_numbers[:players].collect do |player|
+    player[:number]
+  end
+end
